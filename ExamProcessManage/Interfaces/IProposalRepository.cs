b09542a1fa5d016fde56017c6f7f2219867811bd
doannc02 <sillver47108@gmail.com>
2024-09-26 18:33:31 +0700

    update interface proposals

---
+++ ExamProcessManage/Interfaces/IProposalRepository.cs
@@ -0,0 +1,15 @@
+﻿using ExamProcessManage.Dtos;
+using ExamProcessManage.Helpers;
+using ExamProcessManage.ResponseModels;
+
+namespace ExamProcessManage.Interfaces
+{
+    public interface IProposalRepository
+    {
+        Task<PageResponse<ProposalDTO>> GetListProposalsAsync(QueryObject queryObject);
+        Task<BaseResponse<ProposaResponse>> GetDetailProposalAsync(int id);
+        Task<BaseResponse<BaseResponseId>> CreateProposalAsync(AcademicYearResponse academicYear);
+        Task<BaseResponse<BaseResponseId>> UpdateProposalAsync(AcademicYearResponse academicYear);
+        Task<BaseResponse<BaseResponseId>> DeleteProposalAsync(int yearId);
+    }
+}
